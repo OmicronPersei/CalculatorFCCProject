c476b649c60dc4b86b8b6d87a3ed046f63219df6
OmicronPersei <wheeler.weise@gmail.com>
2017-04-08 15:36:30 -0500

    Initial commit.

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+/*global $, jQuery, navigator*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 2, maxerr: 50, white: true  */
+
+function Calculator(DOMID) {
+  "use strict";
+  
+  this.DOMID = DOMID;
+}
